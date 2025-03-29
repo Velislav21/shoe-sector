d49e42cfdac3ef4f65078cbddc4e0a79c231ddcb
Velislav21 <zirovcho34@gmail.com>
2025-03-29 16:55:20 +0200

    adding cart context.

---
+++ client/src/context/CartContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useEffect, useReducer } from "react";
+import requester from "../utils/requester";
+import { BASE_URL } from "../constants/constants";
+
+export const CartContext = createContext();
+
+const initialState = {
+    cart: [],
+    isPending: false,
+    error: null
+}
+
+export function cartReducer(state, action) {
+
+    switch (action.type) {
+
+        case "PENDING":
+            return { ...state, isPending: true, error: null }
+
+        case "GET_CART":
+            return { ...state, cart: action.payload, isPending: false }
+
+        case "CLEAR_CART":
+            return {...state, cart: [], isPending: false}
+
+        case "FAILED_TO_GET_CART":
+            return { ...state, isPending: false, error: action.payload }
+        default:
+            return state;
+    }
+
+}
+
+export function CartContextProvider({ children }) {
+    const [state, dispatch] = useReducer(cartReducer, initialState)
+
+    console.log("Cart Context State:", state);
+
+    return (
+        <CartContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
