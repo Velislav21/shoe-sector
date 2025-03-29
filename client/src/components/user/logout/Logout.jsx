4105fa48768634e50b32c33c177cb3bd9c2410ec
Velislav21 <zirovcho34@gmail.com>
2025-03-29 16:55:31 +0200

    on logout clear cart state

--- client/src/components/user/logout/Logout.jsx
+++ client/src/components/user/logout/Logout.jsx
@@ -1,13 +1,16 @@
 import { Navigate } from "react-router";
 import { useLogout } from "../../../api/usersApi";
 import { useEffect } from "react";
+import { useCartContext } from "../../../hooks/useCartContext";
 
 export default function Logout() {
 
     const { logout } = useLogout();
+    const { _, dispatch } = useCartContext();
 
     useEffect(() => {
         logout();
+        dispatch({type: "CLEAR_CART", payload: null})
     }, [])
 
     return <Navigate to="/login" />
