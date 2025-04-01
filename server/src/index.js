9d8d672fdb2634fa9ef1b66f6b500291a0ed6bba
Velislav21 <zirovcho34@gmail.com>
2025-04-01 15:39:32 +0300

    deploying the server with docker / gcp

--- server/src/index.js
+++ server/src/index.js
@@ -6,13 +6,14 @@ import 'dotenv/config'
 import { authMiddleware } from './middlewares/authMiddleware.js';
 const app = express();
 
+// if need to run locally * mongodb://localhost:27017 * {dbName: shoe-sector}
 const uri = process.env.MONGO_CONNECTION_STRING;
 mongoose.connect(uri)
     .then(() => console.log('Db successfully connected'))
     .catch((err) => console.log(`DB failed: ${err}`))
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*', // 'http://localhost:5173'
     credentials: true,
 }))
 
@@ -20,4 +21,4 @@ app.use(express.json())
 app.use(authMiddleware)
 app.use(routes)
 
-app.listen(3000, () => console.log(`Server is listening on http://localhost:${process.env.PORT || 3000}`))
+app.listen(8080, () => console.log(`Server is listening on http://localhost:${process.env.PORT || 8080}`))
